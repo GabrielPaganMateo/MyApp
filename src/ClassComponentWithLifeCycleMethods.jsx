94787a393a6a27682f127cc4c32925409af94bc5
GabrielPaganMateo <gabriel.pagan.mateo@gmail.com>
2025-07-15 05:57:38 -0400

    comments

--- src/ClassComponentWithLifeCycleMethods.jsx
+++ src/ClassComponentWithLifeCycleMethods.jsx
@@ -2,14 +2,17 @@ import { Component } from 'react';
 
 class ClassComponentWithLifeCycleMethods extends Component {
 
+    // After initial render
     componentDidMount() {
         console.log('Mounted!');
     }
 
+    // After every update or specific state change
     componentDidUpdate(prevProps, prevState) {
         console.log(`Updated! ${prevProps} ${prevState}`);
     }
 
+    // Before unmount
     componentWillUnmount() {
         console.log('Unmounting...');
     }
